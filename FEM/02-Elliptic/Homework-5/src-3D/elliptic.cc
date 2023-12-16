8fe7d767d80d55f0b902a7672dbd473ee0a62668
wenchong <wchung.huang@gmail.com>
2023-12-16 17:01:41 +0800

    finished hw5

---
+++ FEM/02-Elliptic/Homework-5/src-3D/elliptic.cc
@@ -0,0 +1,462 @@
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_out.h>
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/dofs/dof_tools.h>
+#include <deal.II/fe/mapping_fe.h>
+#include <deal.II/dofs/dof_renumbering.h>
+
+#include <deal.II/fe/fe_values.h>
+#include <deal.II/base/quadrature_lib.h>
+ 
+#include <deal.II/base/function.h>
+#include <deal.II/numerics/vector_tools.h>
+#include <deal.II/numerics/matrix_tools.h>
+ 
+#include <deal.II/lac/vector.h>
+#include <deal.II/lac/full_matrix.h>
+#include <deal.II/lac/sparse_matrix.h>
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/solver_cg.h>
+#include <deal.II/lac/precondition.h>
+ 
+#include <deal.II/numerics/data_out.h>
+
+#include <fstream>
+#include <chrono>
+
+struct CPUTimer
+{
+  using HRC = std::chrono::high_resolution_clock;
+  std::chrono::time_point<HRC>  start;
+  CPUTimer() { reset(); }
+  void reset() { start = HRC::now(); }
+  double operator() () const {
+    std::chrono::duration<double> e = HRC::now() - start;
+    return e.count();
+  }
+};
+
+using namespace dealii;
+
+int fineast_level, level_num;
+
+struct Vertex
+{
+  Point<3> pos;
+  unsigned idx;
+
+  int dcmp(const double &a, const double &b) const
+  {
+    if(fabs(a-b) < 1e-12) return 0;
+    else if(a < b) return -1;
+    else return 1;
+  }
+
+  bool operator < (const Vertex &rhs) const
+  {
+    return dcmp(pos[0], rhs.pos[0])==-1
+        || ( dcmp(pos[0], rhs.pos[0])==0
+          && dcmp(pos[1], rhs.pos[1])==-1 )
+        || ( dcmp(pos[0], rhs.pos[0])==0 && dcmp(pos[1], rhs.pos[1])==0
+          && dcmp(pos[2], rhs.pos[2])==-1);
+  }
+};
+
+
+class HiercharchicalPreconditioner : public Subscriptor
+{
+public:
+  HiercharchicalPreconditioner(const int& fine_level,
+                               const int& fine_n_dofs,
+                               const SparseMatrix<double>& fine_system_matrix);
+  void initialize();
+  void vmult(Vector<double> &dst, const Vector<double> &src) const;
+
+private:
+  int level, pow2level;
+
+  Triangulation<3> triangulation;
+  FE_Q<3>   fe;
+  DoFHandler<3>    dof_handler;
+  std::map<types::global_dof_index, Point<3>> dof_location_map;
+
+  AffineConstraints<double> constraints;
+ 
+  SparsityPattern      sparsity_pattern;
+  SparseMatrix<double> system_matrix;
+ 
+  Vector<double> diag_D;
+  HiercharchicalPreconditioner *preconditioner;
+
+  unsigned corse2fine(const unsigned &i) const{
+    return 2*(1+2*pow2level)*(1+2*pow2level)*(i/((1+pow2level)*(1+pow2level))) 
+           + 2*(1+2*pow2level)*(i/(1+pow2level)%(1+pow2level))
+           + 2*(i%(1+pow2level));
+  }
+};
+
+
+HiercharchicalPreconditioner::
+  HiercharchicalPreconditioner(const int& fine_level,
+                               const int& fine_n_dofs,
+                               const SparseMatrix<double>& fine_system_matrix):
+  level(fine_level-1),
+  pow2level(1<<level),
+  fe(1),
+  dof_handler(triangulation)
+{
+  diag_D.reinit(fine_n_dofs);
+  for(int i = 0; i < fine_n_dofs; i++)
+    diag_D[i] = fine_system_matrix(i, i);
+}
+
+
+void HiercharchicalPreconditioner::initialize()
+{
+  GridGenerator::hyper_cube(triangulation);
+  triangulation.refine_global(level);
+  dof_handler.distribute_dofs(fe);
+  dof_location_map = DoFTools::map_dofs_to_support_points(MappingFE<3>(fe), dof_handler);
+  
+  // Renumber the fucking DoFs !!!
+  std::vector<Vertex> fucking_dofs(dof_handler.n_dofs());
+  for(unsigned i = 0; i < fucking_dofs.size(); i++)
+  {
+    fucking_dofs[i].pos = dof_location_map[i];
+    fucking_dofs[i].idx = i;
+  }
+  std::sort(fucking_dofs.begin(), fucking_dofs.end());
+  std::vector< types::global_dof_index > new_numbers(dof_handler.n_dofs());
+  for(unsigned i = 0; i < fucking_dofs.size(); i++)
+    new_numbers[fucking_dofs[i].idx] = i;
+  dof_handler.renumber_dofs(new_numbers);
+  dof_location_map = DoFTools::map_dofs_to_support_points(MappingFE<3>(fe), dof_handler);
+
+  // Add the homogeneous Dirichlet constraint
+  constraints.clear();
+  VectorTools::interpolate_boundary_values(dof_handler,
+                                           0,
+                                           Functions::ZeroFunction<3>(),
+                                           constraints);
+  constraints.close();
+
+  DynamicSparsityPattern dynamic_sparsity_pattern(dof_handler.n_dofs(),
+                                                  dof_handler.n_dofs());
+  DoFTools::make_sparsity_pattern(dof_handler, dynamic_sparsity_pattern);
+  sparsity_pattern.copy_from(dynamic_sparsity_pattern);
+  system_matrix.reinit(sparsity_pattern);
+  MatrixCreator::create_laplace_matrix(dof_handler,
+                                      QGauss<3>(fe.degree),
+                                      system_matrix);
+  constraints.condense(system_matrix);
+  
+  for(unsigned i = 0; i < dof_handler.n_dofs(); i++)
+    diag_D[corse2fine(i)] = 0;
+  
+  if(level >= 4 && level > fineast_level - level_num + 1){
+    preconditioner = new HiercharchicalPreconditioner(level, dof_handler.n_dofs(), system_matrix);
+    preconditioner->initialize();
+  } else {
+    preconditioner = nullptr;
+  }
+}
+
+
+void HiercharchicalPreconditioner::
+  vmult(Vector<double> &dst, const Vector<double> &src) const
+{
+  Vector<double> system_rhs;
+  system_rhs.reinit(dof_handler.n_dofs());
+
+  Vector<double> solution;
+  solution.reinit(dof_handler.n_dofs());
+  
+  // full-weighting
+  const unsigned pw = (pow2level<<1) + 1;
+  const unsigned pw2 = pw * pw;
+
+  auto it = dof_location_map.begin();
+  for(unsigned i = 0; i < system_rhs.size(); i++, it++){
+    int cf = corse2fine(i);
+    const auto& pos = it->second;
+    for(int dx = -1; dx <= 1; dx++){
+      if( (dx==-1 && pos[0]<1e-8) || (dx==1 && pos[0]>1-1e-8) ) continue;
+      for(int dy = -1; dy <= 1; dy++){
+        if( (dy==-1 && pos[1]<1e-8) || (dy==1 && pos[1]>1-1e-8) ) continue;
+        for(int dz = -1; dz <= 1; dz++){
+          if( (dz==-1 && pos[2]<1e-8) || (dz==1 && pos[2]>1-1e-8) ) continue;
+          int weight = 1 << (abs(dx) + abs(dy) + abs(dz));
+          system_rhs[i] += src[cf + dx*pw2 + dy*pw + dz] * 1./weight * 0.125;
+        }
+      }
+    }
+  }
+  
+  SolverControl            solver_control(2000, 1e-4 * system_rhs.l2_norm());
+  SolverCG<Vector<double>> solver(solver_control);
+
+  constraints.condense(system_rhs);
+  if(preconditioner == nullptr)
+    solver.solve(system_matrix, solution, system_rhs, PreconditionIdentity());
+  else
+    solver.solve(system_matrix, solution, system_rhs, *preconditioner);
+  constraints.distribute(solution);
+
+  // std::cout << "     " << solver_control.last_step()
+  //           << " inner CG iterations." << std::endl;
+
+  dst.reinit(src.size());
+  for(unsigned i = 0; i < dst.size(); i++)
+    if(diag_D[i]) dst[i] = src[i] / diag_D[i];
+  
+  // linear interpolation
+  it = dof_location_map.begin();
+  for(unsigned i = 0; i < system_rhs.size(); i++, it++){
+    int cf = corse2fine(i);
+    const auto& pos = it->second;
+    for(int dx = -1; dx <= 1; dx++){
+      if( (dx==-1 && pos[0]<1e-8) || (dx==1 && pos[0]>1-1e-8) ) continue;
+      for(int dy = -1; dy <= 1; dy++){
+        if( (dy==-1 && pos[1]<1e-8) || (dy==1 && pos[1]>1-1e-8) ) continue;
+        for(int dz = -1; dz <= 1; dz++){
+          if( (dz==-1 && pos[2]<1e-8) || (dz==1 && pos[2]>1-1e-8) ) continue;
+          int weight = 1 << (abs(dx) + abs(dy) + abs(dz));
+          dst[cf + dx*pw2 + dy*pw + dz] += 1./weight * solution[i];
+        }
+      }
+    }
+  }
+}
+
+
+class RHS : public Function<3>{
+public:
+  double value(const Point<3> & p, const unsigned int component = 0) const override{
+    (void)component;
+    return 30. * M_PI * M_PI * sin(M_PI*p[0]) * sin(M_PI*p[1]) * sin(M_PI*p[2]);
+  };
+};
+
+
+class Solution : public Function<3>{
+public:
+  double value(const Point<3> & p, const unsigned int component = 0) const override{
+    (void)component;
+    return 10. * sin(M_PI*p[0]) * sin(M_PI*p[1]) * sin(M_PI*p[2]);
+  };
+
+  virtual Tensor<1, 3>
+  gradient(const Point<3> & p, const unsigned int component = 0) const override{
+    (void)component;
+    Tensor<1, 3> grad;
+    grad[0] = 10. * M_PI * cos(M_PI*p[0]) * sin(M_PI*p[1]) * sin(M_PI*p[2]);
+    grad[1] = 10. * M_PI * sin(M_PI*p[0]) * cos(M_PI*p[1]) * sin(M_PI*p[2]);
+    grad[2] = 10. * M_PI * sin(M_PI*p[0]) * sin(M_PI*p[1]) * cos(M_PI*p[2]);
+    return grad;
+  };
+};
+
+
+class Elliptic{
+public:
+  Elliptic(const int &level);
+  void run();
+
+private:
+  void setup_system();
+  void assemble_system();
+  void solve();
+  void output_results() const;
+
+  int level;
+
+  Triangulation<3> triangulation;
+  FE_Q<3>   fe;
+  DoFHandler<3>    dof_handler;
+  std::map<types::global_dof_index, Point<3>> dof_location_map;
+
+  AffineConstraints<double> constraints;
+ 
+  SparsityPattern      sparsity_pattern;
+  SparseMatrix<double> system_matrix;
+ 
+  Vector<double> solution;
+  Vector<double> error;
+  Vector<double> system_rhs;
+
+  CPUTimer timer;
+};
+
+
+Elliptic::Elliptic(const int &level): 
+  level(level),
+  fe(1), 
+  dof_handler(triangulation)
+{}
+
+
+void Elliptic::setup_system(){
+  GridGenerator::hyper_cube(triangulation);
+  triangulation.refine_global(level);
+  dof_handler.distribute_dofs(fe);
+  std::cout << "DoF number: " << dof_handler.n_dofs() << std::endl;
+  dof_location_map = DoFTools::map_dofs_to_support_points(MappingFE<3>(fe), dof_handler);
+  
+  // Renumber the fucking DoFs !!!
+  std::vector<Vertex> fucking_dofs(dof_handler.n_dofs());
+  for(unsigned i = 0; i < fucking_dofs.size(); i++)
+  {
+    fucking_dofs[i].pos = dof_location_map[i];
+    fucking_dofs[i].idx = i;
+  }
+  std::sort(fucking_dofs.begin(), fucking_dofs.end());
+  std::vector< types::global_dof_index > new_numbers(dof_handler.n_dofs());
+  for(unsigned i = 0; i < fucking_dofs.size(); i++)
+    new_numbers[fucking_dofs[i].idx] = i;
+  dof_handler.renumber_dofs(new_numbers);
+  dof_location_map = DoFTools::map_dofs_to_support_points(MappingFE<3>(fe), dof_handler);
+
+  // Add the homogeneous Dirichlet constraint
+  constraints.clear();
+  VectorTools::interpolate_boundary_values(dof_handler,
+                                           0,
+                                           Functions::ZeroFunction<3>(),
+                                           constraints);
+  constraints.close();
+
+  DynamicSparsityPattern dynamic_sparsity_pattern(dof_handler.n_dofs(),
+                                                  dof_handler.n_dofs());
+  DoFTools::make_sparsity_pattern(dof_handler, dynamic_sparsity_pattern);
+  sparsity_pattern.copy_from(dynamic_sparsity_pattern);
+
+  system_matrix.reinit(sparsity_pattern);
+  solution.reinit(dof_handler.n_dofs());
+  error.reinit(dof_handler.n_dofs());
+  system_rhs.reinit(dof_handler.n_dofs());
+
+  MatrixCreator::create_laplace_matrix(dof_handler,
+                                      QGauss<3>(fe.degree+1),
+                                      system_matrix);
+  VectorTools::create_right_hand_side(dof_handler,
+                                      QGauss<3>(fe.degree+1),
+                                      RHS(),
+                                      system_rhs);
+  constraints.condense(system_matrix, system_rhs);
+}
+
+
+void Elliptic::solve()
+{
+  SolverControl            solver_control(20000, 1e-6 * system_rhs.l2_norm());
+  SolverCG<Vector<double>> solver(solver_control);
+  
+  if(level_num > 1)
+  { // Solve with multi-grid preconditioned CG
+    HiercharchicalPreconditioner preconditioner(level,
+                                                dof_handler.n_dofs(),
+                                                system_matrix);
+    preconditioner.initialize();
+    std::cout << "Pre-processed in " << timer() << "s." << std::endl;
+    timer.reset();
+    solver.solve(system_matrix, solution, system_rhs, preconditioner);
+  }
+  else
+  { // Solve with naive CG
+    std::cout << "Pre-processed in " << timer() << "s." << std::endl;
+    timer.reset();
+    solver.solve(system_matrix, solution, system_rhs, PreconditionIdentity());
+  }
+  
+  constraints.distribute(solution);
+  std::cout << "   " << solver_control.last_step()
+            << " CG iterations." << std::endl;
+  std::cout << "Solved in " << timer() << "s." << std::endl;
+  timer.reset();
+
+  Solution u;
+  for(unsigned i = 0; i < solution.size(); i++){
+    error[i] = solution[i] - u.value(dof_location_map[i]);
+  }
+}
+
+
+void Elliptic::output_results() const
+{
+  DataOut<3> data_out;
+  data_out.attach_dof_handler(dof_handler);
+  data_out.add_data_vector(solution, "solution");
+  data_out.add_data_vector(error, "error");
+  data_out.build_patches();
+  std::ofstream output("solution.vtk");
+  data_out.write_vtk(output);
+
+  Vector<double> difference_per_cell(triangulation.n_active_cells());
+
+  VectorTools::integrate_difference(MappingFE<3>(fe),
+                                    dof_handler,
+                                    solution,
+                                    Solution(),
+                                    difference_per_cell,
+                                    QGauss<3>(fe.degree + 1),
+                                    VectorTools::L2_norm);
+  const double L2_error =
+    VectorTools::compute_global_error(triangulation,
+                                      difference_per_cell,
+                                      VectorTools::L2_norm);
+  std::cout << "   L2-norm error: " << L2_error << std::endl;
+
+  difference_per_cell.reinit(triangulation.n_active_cells());
+  const QTrapezoid<1>  q_trapez;
+  const QIterated<3> q_iterated(q_trapez, fe.degree*2+1);
+  VectorTools::integrate_difference(MappingFE<3>(fe),
+                                    dof_handler,
+                                    solution,
+                                    Solution(),
+                                    difference_per_cell,
+                                    q_iterated,
+                                    VectorTools::Linfty_norm);
+  const double Linf_error =
+    VectorTools::compute_global_error(triangulation,
+                                      difference_per_cell,
+                                      VectorTools::Linfty_norm);
+  std::cout << "   Linfty-norm error: " << Linf_error << std::endl;
+
+  difference_per_cell.reinit(triangulation.n_active_cells());
+  VectorTools::integrate_difference(MappingFE<3>(fe),
+                                    dof_handler,
+                                    solution,
+                                    Solution(),
+                                    difference_per_cell,
+                                    QGauss<3>(fe.degree + 1),
+                                    VectorTools::H1_norm);
+  const double H1_error =
+    VectorTools::compute_global_error(triangulation,
+                                      difference_per_cell,
+                                      VectorTools::H1_norm);
+  std::cout << "   H1-norm error: " << H1_error << std::endl;
+  std::cout << "Post-processed in " << timer() << "s." << std::endl;
+}
+
+
+void Elliptic::run()
+{
+  setup_system();
+  solve();
+  output_results();
+}
+
+ 
+int main(int argc, char* argv[])
+{
+  if(argc < 2){
+    std::cerr << "Param error!" << std::endl;
+    return -1;
+  }
+  fineast_level = std::stoi(argv[1]);
+  level_num = (argc >= 3) ? std::stoi(argv[2]) : 1;
+  Elliptic solver(fineast_level);
+  solver.run();
+  return 0;
+}
