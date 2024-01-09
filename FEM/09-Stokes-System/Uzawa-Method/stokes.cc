864420e3eb469dfd57c692ecfd0a4a0541edacad
wenchong <wchung.huang@gmail.com>
2024-01-09 16:15:21 +0800

    add syntax sugar

--- FEM/09-Stokes-System/Uzawa-Method/stokes.cc
+++ FEM/09-Stokes-System/Uzawa-Method/stokes.cc
@@ -21,6 +21,8 @@
 #include <deal.II/lac/precondition.h>
 #include <deal.II/lac/affine_constraints.h>
 #include <deal.II/lac/generic_linear_algebra.h>
+#include <deal.II/lac/linear_operator.h>
+#include <deal.II/lac/packaged_operation.h>
 
 #include <deal.II/grid/tria.h>
 #include <deal.II/grid/grid_generator.h>
@@ -88,18 +90,16 @@ private:
   DoFHandler<dim>    dof_handler;
 
   BlockSparsityPattern      sparsity_pattern;
-  BlockSparsityPattern      preconditioner_sparsity_pattern;
   BlockSparseMatrix<double> system_matrix;
-  BlockSparseMatrix<double> preconditioner_matrix;
 
   BlockVector<double> system_rhs;
   BlockVector<double> solution;
-  BlockVector<double> checker, tmpBV;
+  BlockVector<double> checker;
+  BlockVector<double> block_diag_MQ;
 
   AffineConstraints<double> constraints;
 
   std::shared_ptr<TrilinosWrappers::PreconditionAMG> A_preconditioner;
-  SparseMatrix<double>  diag_MQ_inverse;
 };
 
 
@@ -225,7 +225,7 @@ void StokesProblem<dim>::run()
 {
   make_grid();
   CPUTimer timer;
-  for(unsigned cycle = 0; cycle < level; cycle++)
+  for(unsigned cycle = 0; cycle+2 <= level; cycle++)
   {
     std::cout << "Level: " << cycle+2 << "----------------------------------" << std::endl;
     if(cycle) refine_grid();
@@ -266,10 +266,6 @@ void StokesProblem<dim>::refine_grid()
 template<int dim>
 void StokesProblem<dim>::setup_system()
 {
-  A_preconditioner.reset();
-  system_matrix.clear();
-  preconditioner_matrix.clear();
-
   dof_handler.distribute_dofs(fe);
   DoFRenumbering::Cuthill_McKee(dof_handler);
 
@@ -314,36 +310,21 @@ void StokesProblem<dim>::setup_system()
       dof_handler, coupling, dsp, constraints, false);
     sparsity_pattern.copy_from(dsp);
   }
-  {
-    BlockDynamicSparsityPattern dsp(dofs_per_block, dofs_per_block);
-
-    Table<2, DoFTools::Coupling> coupling(dim+1, dim+1);
-    for(unsigned i = 0; i <= dim; i++)
-      for(unsigned j = 0; j <= dim; j++)
-        coupling[i][j] = DoFTools::none;
-    
-    DoFTools::make_sparsity_pattern(
-      dof_handler, coupling, dsp, constraints, false);
-    preconditioner_sparsity_pattern.copy_from(dsp);
-  }
-
   system_matrix.reinit(sparsity_pattern);
-  preconditioner_matrix.reinit(preconditioner_sparsity_pattern);
-  diag_MQ_inverse.reinit(preconditioner_sparsity_pattern.block(1,1));
 
   solution.reinit(dofs_per_block);
   system_rhs.reinit(dofs_per_block);
   checker.reinit(dofs_per_block);
-  tmpBV.reinit(dofs_per_block);
+  block_diag_MQ.reinit(dofs_per_block);
 }
 
 
 template<int dim>
 void StokesProblem<dim>::assemble_system()
 {
-  system_matrix    = 0;
-  system_rhs       = 0;
-  preconditioner_matrix = 0;
+  system_matrix   = 0;
+  system_rhs      = 0;
+  block_diag_MQ   = 0;
 
   QGaussSimplex<dim> quadrature_formula(degree + 1);
 
@@ -357,9 +338,8 @@ void StokesProblem<dim>::assemble_system()
   const unsigned int n_q_points = quadrature_formula.size();
 
   FullMatrix<double> local_matrix(dofs_per_cell, dofs_per_cell);
-  FullMatrix<double> local_preconditioner_matrix(dofs_per_cell,
-                                                  dofs_per_cell);
   Vector<double>     local_rhs(dofs_per_cell);
+  Vector<double>     local_diag_MQ(dofs_per_cell);
 
   std::vector<types::global_dof_index> local_dof_indices(dofs_per_cell);
 
@@ -377,9 +357,9 @@ void StokesProblem<dim>::assemble_system()
   for (const auto &cell : dof_handler.active_cell_iterators())
     {
       fe_values.reinit(cell);
-      local_matrix                = 0;
-      local_preconditioner_matrix = 0;
-      local_rhs                   = 0;
+      local_matrix    = 0;
+      local_diag_MQ   = 0;
+      local_rhs       = 0;
 
       right_hand_side.value_list(fe_values.get_quadrature_points(),
                                   rhs_values);
@@ -409,7 +389,7 @@ void StokesProblem<dim>::assemble_system()
               local_rhs(i) += phi_u[i]            // phi_u_i(x_q)
                               * rhs_values[q]     // * f(x_q)
                               * fe_values.JxW(q); // * dx
-              local_preconditioner_matrix(i, i) +=
+              local_diag_MQ(i) +=
                     (phi_p[i] * phi_p[i]) // (4)
                     * fe_values.JxW(q);   // * dx
             }
@@ -425,11 +405,9 @@ void StokesProblem<dim>::assemble_system()
                                               local_dof_indices,
                                               system_matrix,
                                               system_rhs);
-      constraints.distribute_local_to_global(local_preconditioner_matrix,
+      constraints.distribute_local_to_global(local_diag_MQ,
                                               local_dof_indices,
-                                              preconditioner_matrix);
-      // for (const unsigned int i : fe_values.dof_indices())
-      //   system_rhs(local_dof_indices[i]) += local_rhs(i);
+                                              block_diag_MQ);
     }
 }
 
@@ -442,42 +420,42 @@ void StokesProblem<dim>::solve()
   additional_data.higher_order_elements = true;
   additional_data.w_cycle = true;
 
+  A_preconditioner.reset();
   A_preconditioner = 
     std::make_shared<TrilinosWrappers::PreconditionAMG>();
   A_preconditioner->initialize(system_matrix.block(0,0), additional_data);
 
-  diag_MQ_inverse.copy_from(preconditioner_matrix.block(1,1));
-  for(auto& p : diag_MQ_inverse)
-    p.value() = 1./p.value();
+  auto diag_MQ_inverse = block_diag_MQ.block(1);
+  for(auto& p : diag_MQ_inverse) p = 1. / p;
 
   // Uzawa iteration
   Vector<double> rhs(solution.block(0).size());
-  Vector<double> prevres(solution.block(0).size());
-  Vector<double> tmp(solution.block(1).size());
-  Vector<double> tmp2(solution.block(1).size());
+  Vector<double> correction_u(solution.block(0).size());
+  Vector<double> correction_p(solution.block(1).size());
   int cnt = 0;
 
-  while(1){
-    cnt++;
-    system_matrix.block(0,1).vmult(rhs, solution.block(1));
-    rhs *= -1;
-    rhs.add(1., system_rhs.block(0));
+  const auto B      = linear_operator(system_matrix.block(1,0));
+  const auto BT     = linear_operator(system_matrix.block(0,1));
+  const auto A      = linear_operator(system_matrix.block(0,0));
+  const auto & f = system_rhs.block(0);
+  auto & u = solution.block(0);
+  auto & p = solution.block(1);
 
-    system_matrix.block(0,0).vmult(prevres, solution.block(0));
-    rhs.add(-1., prevres);
-    prevres = 0.;
-    A_preconditioner->vmult(prevres, rhs);
-    solution.block(0).add(1., prevres);
+  do{
+    cnt++;
+    rhs = f - BT * p - A * u;
+    A_preconditioner->vmult(correction_u, rhs);
+    u += correction_u;
     constraints.distribute(solution);
 
-    system_matrix.block(1,0).vmult(tmp, solution.block(0));
-    diag_MQ_inverse.vmult(tmp2, tmp);
-    solution.block(1).add(1, tmp2);
+    correction_p = B * u;
+    for(unsigned i = 0; i < p.size(); i++)
+      correction_p[i] *= diag_MQ_inverse[i];
+    p += correction_p;
 
     system_matrix.vmult(checker, solution);
     checker.add(-1, system_rhs);
-    if(checker.l2_norm() < 1e-7*system_rhs.l2_norm()) break;
-  }
+  }while(checker.l2_norm() >= 1e-7*system_rhs.l2_norm());
 
   std::cout << "  " << cnt << " Uzawa iterations." << std::endl;
 }
